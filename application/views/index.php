cf6907155c604ad4fca2169e967f666bdb63e2a6
Buchori <muhammad.buchori13@gmail.com>
2022-07-03 18:22:00 +0700

    update view style

--- application/views/index.php
+++ application/views/index.php
@@ -5,55 +5,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login Demo</title>
 
-    <!-- Bootstrap 5 cdn -->
+    <!-- Vendor CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <!-- style -->
     <link rel="stylesheet" href="<?=base_url('assets/css/styles.css')?>">
+
   </head>
   <body>
     
     <!-- Login Form -->
     <section class="login-f">
         <div class="container">
-            <div class="col-lg-10 col-md-12 mx-auto">
+            <div class="col-lg-10 col-md-12 mx-auto card border-0 shadow">
                 <div class="row">
-                    <div class="col-12 ">
-                        <div class="card border-0 shadow rounded-3 my-5">
+                    <div class="col-md-4 sm-none ">
+                        <div class="img-ill">
+                            <img src="<?=base_url('assets/img/ill-login.png')?>" alt="" class="img-responsive">
+                        </div>
+                    </div>
+                    <div class="col-md-8 col-12">
+                        <div class="rounded-3">
                             <div class="card-body p-4 p-sm-5">
                                 <div class="text-title">
                                     <h5>Welcome Back!</h5>
                                     <p>Login to continue</p>
                                 </div>
                                 <form>
-                                    <div class="form-custom mb-3">
+                                    <div class="form-custom">
                                         <i class="fa-solid fa-user"></i>
-                                        <input type="email" class="form-control" placeholder="name@example.com" required>      
+                                        <input type="email" class="form-control" placeholder="Enter Email" required>      
                                     </div>
-                                    <div class="form-custom mb-3">
+                                    <div class="form-custom mb-5">
                                         <i class="fa-solid fa-lock"></i>
-                                        <input type="password" class="form-control" placeholder="Password">
+                                        <input type="password" class="form-control" placeholder="Enter Password" required>
                                     </div>
                                     <div class="grid-btn">
                                         <div class="row">
-                                            <div class="col-6">
-                                                <button class="btn btn-primary btn-block text-uppercase fw-bold" type="submit">Sign in</button>
+                                            <div class="col-md-4 col-6">
+                                                <button class="btn btn-primary btn-block text-uppercase" type="submit">Login</button>
                                             </div>
 
-                                            <div class="col-6">
-                                                <a href="forgotpassword" class="btn btn-secondary btn-block text-uppercase fw-bold"> Forgot Password ?</a>
+                                            <div class="col-md-8 col-6">
+                                                <a href="forgotpassword" class="btn btn-light btn-block text-uppercase"> Forgot Password ?</a>
                                             </div>
                                         </div>
                                     </div>
-                                    <hr class="my-4">
                                 </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            
         </div>
     </section>
     
